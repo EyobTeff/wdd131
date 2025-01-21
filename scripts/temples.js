375777a2ddd29d3c1b42a90cf3e13e1c9fe2731c
eyobt <eyobteff@gmail.com>
2025-01-20 21:32:27 -0700

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,12 @@
 const lastModifiedElement = document.getElementById("last-modified");
 document.getElementById("last-modified").textContent = document.lastModified;
 
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    // Toggle the menu visibility when the hamburger is clicked
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+});
