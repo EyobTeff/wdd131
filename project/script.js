de7018e86071abfb3b748a556c46393abd590f6c
eyobt <eyobteff@gmail.com>
2025-02-13 01:00:36 -0700

    project

---
+++ project/script.js
@@ -0,0 +1,7 @@
+// Display the last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Function to save seasons for later
+function saveForLater(season) {
+  alert(season + " has been added to your 'Save for Later' list!");
+}
